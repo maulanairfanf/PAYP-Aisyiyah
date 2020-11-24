bfdc4e57526450e4215e6e9cc916d8e34e23da64
maulanairfanf <maulanairfanf@gmail.com>
2020-11-24 17:56:21 +0700

    comment component and footer

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <hero></hero>
-        <basic-elements></basic-elements>
+        <!-- <basic-elements></basic-elements>
         <inputs></inputs>
         <custom-controls></custom-controls>
         <navigation></navigation>
@@ -9,34 +9,34 @@
         <icons></icons>
         <examples></examples>
         <download-section></download-section>
-        <carousel></carousel>
+        <carousel></carousel> -->
     </div>
 </template>
 <script>
 import Hero from "./components/Hero";
-import BasicElements from "./components/BasicElements";
-import Inputs from "./components/Inputs";
-import CustomControls from "./components/CustomControls";
-import Navigation from "./components/Navigation";
-import JavascriptComponents from "./components/JavascriptComponents";
-import Carousel from "./components/Carousel";
-import Icons from "./components/Icons";
-import Examples from "./components/Examples";
-import DownloadSection from "./components/DownloadSection";
+// import BasicElements from "./components/BasicElements";
+// import Inputs from "./components/Inputs";
+// import CustomControls from "./components/CustomControls";
+// import Navigation from "./components/Navigation";
+// import JavascriptComponents from "./components/JavascriptComponents";
+// import Carousel from "./components/Carousel";
+// import Icons from "./components/Icons";
+// import Examples from "./components/Examples";
+// import DownloadSection from "./components/DownloadSection";
 
 export default {
   name: "components",
   components: {
     Hero,
-    BasicElements,
-    Inputs,
-    CustomControls,
-    Navigation,
-    JavascriptComponents,
-    Carousel,
-    Icons,
-    Examples,
-    DownloadSection
+    // BasicElements,
+    // Inputs,
+    // CustomControls,
+    // Navigation,
+    // JavascriptComponents,
+    // Carousel,
+    // Icons,
+    // Examples,
+    // DownloadSection
   }
 };
 </script>
--- src/views/components/Hero.vue
+++ src/views/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="section-hero section-shaped my-0">
         <div class="shape shape-style-1 shape-primary">
-            <span class="span-150"></span>
+            <!-- <span class="span-150"></span>
             <span class="span-50"></span>
             <span class="span-50"></span>
             <span class="span-75"></span>
@@ -10,13 +10,13 @@
             <span class="span-50"></span>
             <span class="span-100"></span>
             <span class="span-50"></span>
-            <span class="span-100"></span>
+            <span class="span-100"></span> -->
         </div>
         <div class="container shape-container d-flex align-items-center">
             <div class="col px-0">
                 <div class="row justify-content-center align-items-center">
                     <div class="col-lg-7 text-center pt-lg">
-                        <img src="img/brand/white.png" style="width: 200px;" class="img-fluid">
+                        <!-- <img src="img/brand/white.png" style="width: 200px;" class="img-fluid"> -->
                         <p class="lead text-white mt-4 mb-5">A beautiful Design System for Bootstrap 4. It's Free and Open Source.</p>
                         <div class="btn-wrapper">
                             <base-button tag="a"
