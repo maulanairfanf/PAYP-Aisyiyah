45d627314538bde1631e2a5e8250e99224fa10b9
maulanairfanf <maulanairfanf@gmail.com>
2020-12-12 15:57:11 +0700

    add icon manifest

--- vue.config.js
+++ vue.config.js
@@ -9,13 +9,13 @@ module.exports = {
       })
     ]
   },
-  pwa: {
-    name: 'Vue Argon Design',
-    themeColor: '#172b4d',
-    msTileColor: '#172b4d',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#172b4d'
-  },
+  // pwa: {
+  //   name: 'Vue Argon Design',
+  //   themeColor: '#172b4d',
+  //   msTileColor: '#172b4d',
+  //   appleMobileWebAppCapable: 'yes',
+  //   appleMobileWebAppStatusBarStyle: '#172b4d'
+  // },
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
