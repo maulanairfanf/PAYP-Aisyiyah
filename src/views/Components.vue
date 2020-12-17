0c2a6053701fe9518f5c3e65a47630a9803edf06
maulanairfanf <maulanairfanf@gmail.com>
2020-12-17 23:05:49 +0700

    celan component

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,51 +1,24 @@
 <template>
-    <div>
-        <!-- <hero></hero> -->
-        <home></home>
-        <aboutUs></aboutUs>
-        <kegiatan></kegiatan>
-        <contact></contact>
-        <!-- <inputs></inputs> -->
-        <!-- <custom-controls></custom-controls>
-        <navigation></navigation>
-        <javascript-components></javascript-components>
-        <icons></icons>
-        <examples></examples>
-        <download-section></download-section>
-        <carousel></carousel> -->
-    </div>
+  <div>
+    <home></home>
+    <aboutUs></aboutUs>
+    <kegiatan></kegiatan>
+    <contact></contact>
+  </div>
 </template>
 <script>
-// import Hero from "./components/Hero";
-import Home from "./components/Home"
+import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import Kegiatan from "./components/Kegiatan";
 import Contact from "./components/Contact";
-// import Inputs from "./components/Inputs";
-// import CustomControls from "./components/CustomControls";
-// import Navigation from "./components/Navigation";
-// import JavascriptComponents from "./components/JavascriptComponents";
-// import Carousel from "./components/Carousel";
-// import Icons from "./components/Icons";
-// import Examples from "./components/Examples";
-// import DownloadSection from "./components/DownloadSection";
 
 export default {
   name: "components",
   components: {
-    // Hero,
     Home,
     AboutUs,
     Kegiatan,
     Contact,
-    // Inputs,
-    // CustomControls,
-    // Navigation,
-    // JavascriptComponents,
-    // Carousel,
-    // Icons,
-    // Examples,
-    // DownloadSection
-  }
+  },
 };
 </script>
